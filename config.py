1e978d74efec0a06a1e9845d770a60c503b0cda5
CruiseTian <979376748@qq.com>
2021-06-01 23:42:21 +0800

    add gpu

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ parser = argparse.ArgumentParser(description='Hyper-parameters management')
 # Hardware options
 parser.add_argument('--workers', type=int, default=4,help='number of threads for data loading')
 parser.add_argument('--seed', type=int, default=2021, help='random seed')
-parser.add_argument('--gpu_id', type=list,default=[0,1], help='multi-GPU')
+parser.add_argument('--gpu_id', type=list,default=[0,1,2,3], help='multi-GPU')
 
 # Preprocess parameters
 parser.add_argument('--weight', type=str, default=None, help='model init weight')
@@ -16,7 +16,7 @@ parser.add_argument('--lower', type=int, default=-200, help='')
 # data in/out and dataset
 parser.add_argument('--dataset_path',default = './datasets',help='fixed trainset root path')
 parser.add_argument('--save_path',default='.',help='save path of trained model')
-parser.add_argument('--batch_size', type=list, default=2,help='batch size of trainset')
+parser.add_argument('--batch_size', type=list, default=4,help='batch size of trainset')
 
 # train
 parser.add_argument('--epochs', type=int, default=200, metavar='N',help='number of epochs to train (default: 200)')
