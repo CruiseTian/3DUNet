9f06cdc4d462dc379bc0f21448752a6c226c588a
CruiseTian <979376748@qq.com>
2021-06-11 15:45:39 +0800

    change lr auto

--- utils/common.py
+++ utils/common.py
@@ -57,8 +57,8 @@ def print_network(net):
     print('Total number of parameters: %d' % num_params)
 
 def adjust_learning_rate(optimizer, epoch, args):
-    """Sets the learning rate to the initial LR decayed by 10 every 10 epochs"""
-    lr = args.lr * (0.1 ** (epoch // 20))
+    """Sets the learning rate to the initial LR decayed by 2 every 15 epochs"""
+    lr = args.lr * (0.5 ** (epoch // 15))
     for param_group in optimizer.param_groups:
         param_group['lr'] = lr
 
